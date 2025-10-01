5b6442ccaf85f33695c700e76bb2e84c0fb39903
bariseroglu <baris.eroglu@gmail.com>
2025-10-01 22:36:17 +0300

    feat: add billing-service

---
+++ billing-service/src/test/java/com/energy/simulation/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energy.simulation.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
